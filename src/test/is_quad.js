5506decd974a4af28d1dab89c2b6db130643e227
John Haugeland <stonecypher@gmail.com>
2017-10-21 22:59:00 -0700

    Remove accidental spare copy of testset.  Add a few more erroneous cases.  Create .npmignore.  Trigger build pass, in theory

--- src/test/is_quad.js
+++ src/test/is_quad.js
@@ -12,12 +12,20 @@ const rand_byte = () => rand(256);
 
 describe('Basic quad passes', async _it => {
 
-  const must_pass = tcase => test(`${tcase} passes`, t => t.is(true, is_quad(tcase) ));
+  const must_pass    = tcase => test(`${tcase} passes`, t => t.is(true, is_quad(tcase) ));
+  const must_pass_ex = tcase => test(`${tcase} passes`, t => t.is(true, is_quad_ex(tcase).result ));
 
-  [ '0.0.0.0', '1.1.1.1', '255.255.255.255', '127.0.0.1', '8.8.8.8', '0.255.255.0' ].map(must_pass);
+  const cases = [ '0.0.0.0', '1.1.1.1', '255.255.255.255', '127.0.0.1', '8.8.8.8', '0.255.255.0' ];
+  cases.map(must_pass);
+  cases.map(must_pass_ex);
 
   let counter = 0;
-  while (counter++ < 1000) { must_pass(`${rand_byte()}.${rand_byte()}.${rand_byte()}.${rand_byte()}`); }
+
+  while (counter++ < 1000) {
+    const tcase = `${rand_byte()}.${rand_byte()}.${rand_byte()}.${rand_byte()}`;
+    must_pass(tcase);
+    must_pass_ex(tcase);
+  }
 
 });
 
@@ -27,19 +35,25 @@ describe('Basic quad passes', async _it => {
 
 describe('Basic quad rejects', async _it => {
 
-  const must_fail = tcase => test(`${tcase} fails`, t => t.is(false, is_quad(tcase) ));
+  const must_fail    = tcase => test(`${tcase} fails`, t => t.is(false, is_quad(tcase) ));
+  const must_fail_ex = tcase => test(`${tcase} fails`, t => t.is(false, is_quad_ex(tcase).result ));
 
-  [ '0.0.0',     '0.0.0.0.0',                           // wrong length
+  const cases = [
+    '0.0.0',     '0.0.0.0.0',                           // wrong length
     '256.1.1.1', '1.256.1.1', '1.1.256.1', '1.1.1.256', // out of bounds byte high
     '-1.1.1.1',  '1.-1.1.1',  '1.1.-1.1',  '1.1.1.-1',  // out of bounds byte low
     '255.255.255.one',                                  // letters
+    ' 1.1.1.1', '1.1.1.1 ', '1.1 .1.1', '1.1. 1.1',     // spaces
     '1.1.1.1:80',                                       // port
     '1.1.1.1/8',                                        // subnet
     '.1.1.1', '1..1.1', '1.1..1', '1.1.1.',             // missing byte
     '1.1.1.1.', '.1.1.1.1', '1.1..1.1',                 // spurious leading/trailing/internal period
     '01.1.1.1', '1.01.1.1', '1.1.01.1', '1.1.1.01',     // leading zeroes not-just-zero
-    '1.1.00.1',                                         // leading zeroes on-a-zero
+    '00.1.1.1', '1.00.1.1', '1.1.00.1', '1.1.1.00',     // leading zeroes on-a-zero
     5, false, {}, []                                    // non-strings
-  ].map(must_fail);
+  ];
+
+  cases.map(must_fail);
+  cases.map(must_fail_ex);
 
 });
